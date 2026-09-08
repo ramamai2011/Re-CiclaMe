704a3cf10ffdd0c3774f21a1df48d5b1c37ea53b
Valenn97 <valentinoganci2010@gmail.com>
2026-09-08 15:52:30 -0300

    Empiezo finalmente

---
+++ Code/users.ts
@@ -0,0 +1,14 @@
+// Declaraciones botones
+const correo_input = document.getElementById("correo_input") as HTMLInputElement
+const nombre_usuario_input = document.getElementById("usuario_input") as HTMLInputElement
+const contraseña_input = document.getElementById("password_input") as HTMLInputElement
+const repetir_contraseña_input = document.getElementById("repetir_contraseña") as HTMLInputElement
+const postal_input = document.getElementById("codigo_postal_input") as HTMLInputElement
+const boton_crear_cuenta = document.getElementById("crear_cuenta") as HTMLButtonElement
+
+
+boton_crear_cuenta.addEventListener("click", () => {
+    let correo = correo_input.value
+    let contraseña = contraseña_input.value
+    console.log(correo)
+})
