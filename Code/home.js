7354c03d5f72dfa66d9d736990429fff3cffb865
Mercho <you@example.com>
2026-09-15 12:09:21 -0300

    home2

--- Code/home.js
+++ Code/home.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const datoshome = {
-  usuario: "gordet",
+  usuario: "Gordet",
   puntoCercano: "Belgrano",
   beneficios: "10% en COTO",
   historial: "botella de plástico - 10 puntos",
@@ -10,5 +10,9 @@ const datoshome = {
 };
 
 const recusuario = document.querySelector(".holausu");
-recusuario.innerHTML = "<span class='textousuario'>hola, " + datoshome.usuario + "</span>" + "<span class= 'numeropuntos'> " datoshome.puntos
+recusuario.innerHTML = "<span class='textousuario'>¡Hola, " + datoshome.usuario + '!' + "</span>" +
+ "<span class= 'numeropuntos'> " + datoshome.puntos + "</span>" +
+ "<span class= 'puntospuntos'>Puntos " + "</span>" +
+  "<span class= 'usubarra'></span>"
+
  
